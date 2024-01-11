7032511a63e42454e8af6d46b1aefa608076707d
Matheus Valenzuela <matheuszvalenz@gmail.com>
2024-01-11 18:34:49 +0000

    Optimized code

--- areaOrPerimeter.js
+++ areaOrPerimeter.js
@@ -1,8 +1,13 @@
-const areaOrPerimeter = function(l , w) {
-    if(l !== w){
-      let rectangle  =  (l + w)*2
-      return rectangle
-    } else {
-      return l * w
-    }
-  };
+// const areaOrPerimeter = function(l , w) {
+//     if(l !== w){
+//       let rectangle  =  (l + w)*2
+//       return rectangle
+//     } else {
+//       return l * w
+//     }
+//   };
+
+
+  const areaOrPerimeterOptimized = function(l , w) {
+  return l == w ? l*w : 2*(l + w)
+};
