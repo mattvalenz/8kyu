0365353868153a61dd0f48df77ca0f6bbcaa2dad
Matheus Valenzuela <matheuszvalenz@gmail.com>
2024-01-12 20:48:14 +0000

    Committing optimized version, previous had too many unnecessary lines of code.

--- removeTheMinimum.js
+++ removeTheMinimum.js
@@ -1,15 +1,4 @@
 function removeSmallest(numbers) {
-  let result = [];
-  length = numbers.length;
-  array = [...numbers];
-  count = 0;
-
-  for (let i = 0; i < length; i++) {
-    for (let e = i + e; e < length; e++) {
-      if (array[i] > array[e]) {
-        array[i], (array[e] = array[e]), array[i];
-      }
-    }
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
   }
-
-}
